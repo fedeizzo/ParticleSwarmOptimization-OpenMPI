40ec0be5dc6c9929fba9e426a34dfc2ab4800406
Federico Izzo <federico.izzo99@gmail.com>
2022-10-27 17:19:07 +0200

    add python deps for jupyter notebook benchmarking

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "High Performance Computing course project 2022";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
-
   outputs = { self, nixpkgs }:
     let
       pname = "HPC_program";
@@ -16,14 +15,15 @@
         gnumake
         pkg-config
         glib
+        llvmPackages_13.openmp
       ];
       shellDeps = with pkgs; [
-        python310
-        python310Packages.numpy
-        python310Packages.jupyterlab
-        python310Packages.pandas
-        python310Packages.matplotlib
-        python310Packages.scipy
+        python39
+        python39Packages.numpy
+        python39Packages.jupyterlab
+        python39Packages.pandas
+        python39Packages.matplotlib
+        python39Packages.scipy
         sqlite-web
       ];
       CPackage = pkgs.stdenv.mkDerivation {
@@ -59,7 +59,7 @@
       };
       devShell."${system}" = pkgs.mkShell {
         name = "${pname}_shell";
-        packages = buildDeps ++ runtimeDeps ++ shellDeps;
+        nativeBuildInputs = buildDeps ++ runtimeDeps ++ shellDeps;
       };
     };
 }
