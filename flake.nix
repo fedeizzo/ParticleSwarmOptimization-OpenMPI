4e31861d3e424ae8e8dfd041a3013c5be5f41140
Federico Izzo <federico.izzo99@gmail.com>
2022-11-26 15:30:01 +0100

    add new latex deps

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             adjustbox babel-german background bidi collectbox csquotes everypage filehook
             footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights
             needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem
-            unicode-math upquote xecjk xurl zref;
+            unicode-math upquote xecjk xurl zref algorithms algpseudocodex algorithmicx;
         })
       ];
       shellDeps = with pkgs; [
